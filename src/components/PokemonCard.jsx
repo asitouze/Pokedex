853c89e44290a5868bf560c71478084bc845a310
asitouze <aurelien.sitouze@gmail.com>
2023-11-24 11:58:52 +0100

    test

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,7 @@
+function PokemonCard() {
+   <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"> </img></figure>;
+    <figcaption>Bulbasaur</figcaption>;
+    // return
+  }
+  
+  export default PokemonCard;
