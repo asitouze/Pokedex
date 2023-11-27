8fa0edecacd791b21f3c3aa4984ba530fb87355c
asitouze <aurelien.sitouze@gmail.com>
2023-11-27 10:44:38 +0100

    react3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,27 @@
-function PokemonCard() {
+function PokemonCard() {      
     return (
-    <div>
-        <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/> </figure>
-        <figcaption>Bulbasaur</figcaption>
-    </div>)
+      <div>
+        {pokemonList.map((pokemon, i) => (
+          <div key={i}>
+            <figure>
+              {pokemon.imgSrc ?  <img src={pokemon.imgSrc} /> :<p>???</p>}
+            </figure>
+            <figcaption>{pokemon.name}</figcaption>
+          </div>
+        ))}
+      </div>
+    );
   }
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
   
   export default PokemonCard;
